acc4d1ccdf60cb02b604695724f7d7752d8c777a
Jikuhara <kei2003730@gmail.com>
2025-06-21 21:09:36 +0900

    minilibxを使用したいが、上手く動かない。unreferenceがいくつか出てくるので、それらの原因を突き止めていく。

--- test/sandbox_for_minilibx.c
+++ test/sandbox_for_minilibx.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   sandbox_for_minilibx.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/21 20:45:22 by kjikuhar          #+#    #+#             */
-/*   Updated: 2025/06/21 21:06:36 by kjikuhar         ###   ########.fr       */
+/*   Created: 2025/06/21 21:00:25 by kjikuhar          #+#    #+#             */
+/*   Updated: 2025/06/21 21:08:05 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fdf.h"
+#include <stdio.h>
+#include "../minilibx-linux/mlx.h"
 
 int main(int argc, char const *argv[])
 {
-	mlx_init();
-
+	printf("%p\n", mlx_init());
 	return (0);
 }
